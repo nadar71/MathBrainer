f07efb36b1fd434a571e43d4a11925a1d90cf946
simone <simone.mapelli@gmail.com>
2018-12-07 16:41:25 +0100

    Created Bare bone of Settings Activity
    Create the main structure of
    Settings Activity
    with generic placeholders

--- app/src/main/java/eu/indiewalkabout/mathbrainer/MainActivity.java
+++ app/src/main/java/eu/indiewalkabout/mathbrainer/MainActivity.java
@@ -3,6 +3,8 @@ package eu.indiewalkabout.mathbrainer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -57,4 +59,26 @@ public class MainActivity extends AppCompatActivity {
 
         // END Button management
     }
+
+    // ---------------------------------------------------------------------------------------------
+    //                                          MENU STUFF
+    // ---------------------------------------------------------------------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainactivity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+
+            Intent settingsIntent = new Intent(this, MainSettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
