70b9e9fb839c9848c698597e287dc236a62b5ce7
simone <simone.mapelli@gmail.com>
2019-03-25 12:28:22 +0100

    For mixedop_write_result: + delete status bar above + hide bottom bar below + polishing graphics of in game stats

--- app/src/main/java/eu/indiewalkabout/mathbrainer/aritmetic/MixedOp_Write_Result_Activity.java
+++ app/src/main/java/eu/indiewalkabout/mathbrainer/aritmetic/MixedOp_Write_Result_Activity.java
@@ -167,7 +167,28 @@ public class MixedOp_Write_Result_Activity extends AppCompatActivity implements 
             }
         });
 
+        // make bottom navigation bar and status bar hide
+        hideStatusNavBars();
 
+
+    }
+
+
+
+    /**
+     * -------------------------------------------------------------------------------------------------
+     * Make bottom navigation bar and status bar hide
+     * -------------------------------------------------------------------------------------------------
+     */
+    private void hideStatusNavBars() {
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     /**
