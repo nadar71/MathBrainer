4c6753309d18c72483b2553722eff1a71fc6bace
simone <simone.mapelli@gmail.com>
2019-03-27 10:58:29 +0100

    + #43 : hide status display and bottom bar in count object and number order games

--- app/src/main/java/eu/indiewalkabout/mathbrainer/othergames/NumberOrderActivity.java
+++ app/src/main/java/eu/indiewalkabout/mathbrainer/othergames/NumberOrderActivity.java
@@ -178,9 +178,29 @@ public class NumberOrderActivity extends AppCompatActivity implements IGameFunct
         // set first level
         updateLevel();
 
+        // make bottom navigation bar and status bar hide
+        hideStatusNavBars();
+
     }
 
 
+
+    /**
+     * -------------------------------------------------------------------------------------------------
+     * Make bottom navigation bar and status bar hide, without resize when reappearing
+     * -------------------------------------------------------------------------------------------------
+     */
+    private void hideStatusNavBars() {
+        // minsdk version is 19, no need code for lower api
+        View decorView = getWindow().getDecorView();
+        int uiOptions =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION     // hide navigation bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY  // hide navigation bar
+                        // View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        // View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN; // // hide status bar
+        decorView.setSystemUiVisibility(uiOptions);
+    }
 
 
     /**
