43dcec999b4cb966c16d9b2e89584a8ffa052de2
simone <simone.mapelli@gmail.com>
2019-04-02 11:35:10 +0200

    + #33 : make the main menu interface better : icons

--- app/src/main/java/eu/indiewalkabout/mathbrainer/ChooseGameActivity.java
+++ app/src/main/java/eu/indiewalkabout/mathbrainer/ChooseGameActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.gms.ads.AdView;
 
@@ -43,10 +44,10 @@ public class ChooseGameActivity extends AppCompatActivity {
 
     Button ResultWriteBtn;
     Button ResultChooseBtn;
-    Button quickCountBtn;
+    ImageView quickCountBtn;
     Button doublingBtn;
     Button fallingBtn;
-    Button orderBtn;
+    ImageView orderBtn;
     Button randomOpsBtn;
     Button soundsSeqBtn;
 
@@ -55,7 +56,7 @@ public class ChooseGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_game);
+        setContentView(R.layout.activity_choose_game_new);
 
         mAdView = findViewById(R.id.adView);
 
